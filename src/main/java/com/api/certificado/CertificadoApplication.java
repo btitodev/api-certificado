cd17562c9712fab2ac9774369ed0439cd6ea0e00
btitodev <brunosantostito3@hotmail.com>
2025-07-31 15:55:55 -0300

    feat: configurações

--- src/main/java/com/api/certificado/CertificadoApplication.java
+++ src/main/java/com/api/certificado/CertificadoApplication.java
@@ -1,13 +1,20 @@
 package com.api.certificado;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+import com.api.certificado.controller.SolicitacaoCertificadoController;
 
 @SpringBootApplication
 public class CertificadoApplication {
 
+	private static final Logger logger = LoggerFactory.getLogger(SolicitacaoCertificadoController.class);
+
 	public static void main(String[] args) {
 		System.out.println("Iniciando aplicação Certificado");
+		logger.info("Iniciando aplicação Certificado");
 
 		try {
 			SpringApplication.run(CertificadoApplication.class, args);
--- src/main/java/com/api/certificado/service/PagamentoService.java
+++ src/main/java/com/api/certificado/service/PagamentoService.java
@@ -6,6 +6,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import com.api.certificado.domain.solicitacaoCertificado.StatusSolicitacaoCertificado;
+import com.api.certificado.producer.SolicitacaoCertificadoProducer;
 
 @Service
 public class PagamentoService {
@@ -13,12 +14,18 @@ public class PagamentoService {
     @Autowired
     private SolicitacaoCertificadoService solicitacaoCertificadoService;
 
+    @Autowired
+    private SolicitacaoCertificadoProducer solicitacaoCertificadoProducer;
+
     public void confirmarPagamento(UUID idSolicitacao) {
         // Atualiza o status da solicitação de certificado para "Boleto Pago"
 
         System.out.println("Confirmando pagamento para a solicitação: " + idSolicitacao);
         solicitacaoCertificadoService.updateStatus(idSolicitacao, StatusSolicitacaoCertificado.BOLETO_PAGO);
         System.out.println("Pagamento confirmado para a solicitação: " + idSolicitacao);
+
+        
+
     }
 
 }
